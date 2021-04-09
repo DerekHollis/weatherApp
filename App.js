13f61a96b8ef1773dfee44812bc537692d7df3fc
DerekHollis <70291569+DerekHollis@users.noreply.github.com>
2021-04-09 16:25:18 -0300

    Update App.js

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Master from './Components/Master.jsx';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Master></Master>
     </View>
   );
 }
@@ -19,3 +20,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
